c75c361b934702ac62fd6de11e516c50ae5cf070
myradov <myradov-be@outlook.com>
2022-11-19 17:44:06 +0500

    Working on Main Page

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer>
-        <ul>
+    <footer className='absolute bottom-0 bg-main'>
+        <ul className='flex'>
             <li>
                 <Link href="/">
                     Home
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import Swiper core and required modules
+import { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 
 export default function Home() {
   return (
@@ -10,10 +23,53 @@ export default function Home() {
         <meta name="description" content="Zerur Electronics - Home Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='relative w-full h-auto pt-24'>
-        <h1 className='bg-main'>Zerur</h1>
-        <h1 className='bg-green-400'>Zerur</h1>
-        Hello from Zerur Electronics
+      <section className='relative w-full h-auto pt-20'>
+        <Swiper
+          // install Swiper modules
+          modules={[EffectFade, Autoplay, Navigation, Pagination]}
+          spaceBetween={30}
+          loop={true}
+          effect={"fade"}
+          slidesPerView={1.2}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation={true}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide>
+            <div className='block w-full h-60 md:h-96 '>
+              <Image src="/elec_1.jpg" layout="fill" objectFit="cover"/>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='block w-full h-60 md:h-96 '>
+              <Image src="/elec_2.jpg" layout="fill" objectFit="cover"/>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='block w-full h-60 md:h-96 '>
+              <Image src="/elec_3.jpg" layout="fill" objectFit="cover"/>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='block w-full h-60 md:h-96 '>
+              <Image src="/elec_4.jpg" layout="fill" objectFit="cover"/>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </section>
+      <main className='relative flex flex-col justify-between items-center w-full h-auto'>
+        <section className='bg-gray-200 w-full md:w-1/5 md:mr-1'>
+          Sidebar
+        </section>
+        <section className='bg-gray-400 w-full md:flex-1'>
+          Products
+        </section>
       </main>
     </div>
   )
