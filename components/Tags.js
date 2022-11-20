42fd822b15d86f3b42b14d0a2890509044bb4347
myradov <myradov-be@outlook.com>
2022-11-20 22:22:47 +0500

    Tags grid

--- components/Tags.js
+++ components/Tags.js
@@ -4,10 +4,10 @@ import Image from 'next/image'
 
 export const Tags = ({title}) => {
   return (
-    <Link href="#" className='relative basis-2/5 md:w-full h-8 m-1'>
+    <Link href="#" className='relative col-span-1 md:col-span-2 h-8 m-1'>
         <li className='relative w-full h-full flex justify- items-center flex-nowrap border border-black rounded-md overflow-hidden bg-blue-100'>
             <div className='relative w-1/3'>
-            <Image src="/z_cat4.jpg" layout="responsive" className='w-full object-cover' width={50} height={35}/>
+            <Image src="/z_cat4.jpg"  className='w-full object-cover' width={50} height={35}/>
             </div>
             <p className='relative w-2/3 text-center'>{title}</p>
         </li>
--- pages/index.js
+++ pages/index.js
@@ -46,57 +46,57 @@ export default function Home() {
         >
           <SwiperSlide>
             <div className='block w-full h-52 md:h-96 '>
-              <Image src="/elec_1.jpg" layout="fill" objectFit="cover"/>
+              <Image src="/elec_1.jpg" fill objectFit="cover"/>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className='block w-full h-52 md:h-96 '>
-              <Image src="/elec_2.jpg" layout="fill" objectFit="cover"/>
+              <Image src="/elec_2.jpg" fill objectFit="cover"/>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className='block w-full h-52 md:h-96 '>
-              <Image src="/elec_3.jpg" layout="fill" objectFit="cover"/>
+              <Image src="/elec_3.jpg" fill objectFit="cover"/>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className='block w-full h-52 md:h-96 '>
-              <Image src="/elec_4.jpg" layout="fill" objectFit="cover"/>
+              <Image src="/elec_4.jpg" fill objectFit="cover"/>
             </div>
           </SwiperSlide>
         </Swiper>
       </section>
       <main className='relative flex flex-col md:flex-row justify-between items-start w-full h-auto'>
         <section className='bg-gray-200 w-full md:w-1/5 md:mr-1'>
-          <ul className='relative w-full flex flex-row flex-wrap md:flex-col justify-center items-center p-1'>
-            <Link href="#" className='relative basis-2/5 md:basis-2/5 h-8 m-1'>
+          <ul className='relative w-full grid grid-cols-2 md:grid-cols-1 p-1'>
+            <Link href="#" className='relative col-span-1 md:col-span-2 h-8 m-1'>
               <li className='relative w-full h-full flex justify- items-center flex-nowrap border border-black rounded-md overflow-hidden bg-blue-100'>
                   <div className='relative w-1/3'>
-                    <Image src="/z_cat1.jpg" layout="responsive" className='w-full object-cover' width={50} height={35}/>
+                    <Image src="/z_cat1.jpg" className='w-full object-cover' width={50} height={35}/>
                   </div>
                   <p className='relative w-2/3 text-center'>Electronics</p>
               </li>
             </Link>
-            <Link href="#" className='relative basis-2/5 md:basis-1/3 h-8 m-1'>
+            <Link href="#" className='relative col-span-1 md:col-span-2 h-8 m-1'>
               <li className='relative w-full h-full flex justify- items-center flex-nowrap border border-black rounded-md overflow-hidden bg-blue-100'>
                   <div className='relative w-1/3'>
-                    <Image src="/z_cat2.jpg" layout="responsive" className='w-full object-cover' width={50} height={35}/>
+                    <Image src="/z_cat2.jpg" className='w-full object-cover' width={50} height={35}/>
                   </div>
                   <p className='relative w-2/3 text-center'>Computer</p>
               </li>
             </Link>
-            <Link href="#" className='relative basis-2/5 md:w-full h-8 m-1'>
+            <Link href="#" className='relative col-span-1 md:col-span-2 h-8 m-1'>
               <li className='relative w-full h-full flex justify- items-center flex-nowrap border border-black rounded-md overflow-hidden bg-blue-100'>
                   <div className='relative w-1/3'>
-                    <Image src="/z_cat3.jpg" layout="responsive" className='w-full object-cover' width={50} height={35}/>
+                    <Image src="/z_cat3.jpg" className='w-full object-cover' width={50} height={35}/>
                   </div>
                   <p className='relative w-2/3 text-center'>Accessories</p>
               </li>
             </Link>
-            <Link href="#" className='relative basis-2/5 md:w-full h-8 m-1'>
+            <Link href="#" className='relative col-span-1 md:col-span-2 h-8 m-1'>
               <li className='relative w-full h-full flex justify- items-center flex-nowrap border border-black rounded-md overflow-hidden bg-blue-100'>
                   <div className='relative w-1/3'>
-                    <Image src="/z_cat4.jpg" layout="responsive" className='w-full object-cover' width={50} height={35}/>
+                    <Image src="/z_cat4.jpg" className='w-full object-cover' width={50} height={35}/>
                   </div>
                   <p className='relative w-2/3 text-center'>Phone</p>
               </li>
